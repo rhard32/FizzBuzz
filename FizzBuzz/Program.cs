fc4c8ed73af6cc90789278c8e24e5da0418cdcc9
rhard32 <rhard32@gmail.com>
2015-03-25 16:49:50 -0400

    FizzBuzz
    I cannot figure out how to fix the output for things divisible by 3 and
    5

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,67 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+
+            //FizzBuzz Problem
+
+            for (int n = 0; n < 101; n++)
+            {
+                Console.WriteLine(n);
+
+
+                if ((n % 3 == 0) && (n % 5 == 0))
+                {
+                    Console.WriteLine(n + "FIZZBUZZ");
+                }
+                    if (n % 3 == 0) 
+                    
+                        Console.WriteLine(n + "FIZZ");
+                        if (n % 5 == 0)
+                        {
+                            Console.WriteLine(n + "BUZZ");
+
+
+                         
+                            //Console.ReadLine();
+                        }
+
+
+
+                        //foreach (var item in args)
+                        //{
+                        //    Console.WriteLine(item);
+                        //    for (int i = 0; i < item.Length; i++)
+                        //        Console.WriteLine("\t{0}", item[i]);
+                        //}
+
+                        //while (args.Length > 0)
+                        //{
+                        //    Console.WriteLine(args[0]);
+                        //    int index = 0;
+                        //    do
+                        //    {
+                        //        Console.WriteLine("\t{0}", args[0][index++]);
+                        //    } while (index < args[0].Length);
+                        //    args = args.Skip(1).ToArray();
+                        //}
+
+                        //string message;
+                        //do
+                        //{
+                        //    Console.WriteLine("Write a message");
+                        //    message = Console.ReadLine();
+                        //    Console.WriteLine("Your message is {0}", message);
+                        //} while (message != "quit");
+
+
+
+
+
+
+                    }
+                }
+            }
         }
-    }
-}
+ 
+
+        
